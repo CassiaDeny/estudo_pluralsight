ed9dacf2e8f8821fab54ad4f3589f336c1cb2d07
Cássia Denyelli Moreira <cassia.deny@gmail.com>
2016-12-15 18:13:56 -0200

    adição da aula sobre arrays

--- js_from_fundamentals_to_functional/JS102-master/scripts.js
+++ js_from_fundamentals_to_functional/JS102-master/scripts.js
@@ -1,23 +1,47 @@
 //Use this file to implement Part One of your project
+function objectsAsDataModels(){
 
-var animal = {};
-animal.username = 'DaffDuck';
-animal['tagline'] = 'Yippeee';
+    var animal = {};
+    animal.username = 'DaffDuck';
+    animal['tagline'] = 'Yippeee';
 
-var noises = [];
-animal.noises = noises; 
+    var noises = arraysAsCollections();
+    animal.noises = noises; 
 
-var proCount = 0;
+    var proCount = 0;
 
-for(var key in animal){
-    proCount ++;
+    for(var key in animal){
+        proCount ++;
 
-    if (key == "username"){
-        console.log("Hi my name is " + animal[key] );
+        if (key == "username"){
+            console.log("Hi my name is " + animal[key] );
+        }
+        else if(key == "tagline"){
+            console.log("I like to say " + animal[key]);
+        }
     }
-    else if(key == "tagline"){
-        console.log("I like to say " + animal[key]);
-    }
+
+    console.log("Count " + proCount);
+   console.log(animal);
 }
 
-console.log("Count " + proCount);
+function arraysAsCollections(){
+
+    //cria array
+    var noiseArray = [];
+
+    //insere item no início do array
+    noiseArray.unshift("quack");
+    //insere item no final do array
+    noiseArray.push("honk");
+    //adiciona mais um item com bracket notation
+    noiseArray[3] = "sneeze";
+
+    //qual é o tamanho?
+    console.log("Tamanho do array " + noiseArray.length);
+    console.log("Qual é o último index " + (noiseArray.length - 1));
+
+    return noiseArray;
+}
+
+objectsAsDataModels();
